446190a18ae38e19ce8cbef30f01c66fa240c196
mrd <qzwxecrvtbasdf@gmail.com>
2020-03-23 16:26:13 +0300

    kvitan1

---
+++ kvitan_v1.2/kvitan/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kvitan
+{
+    class kvit
+    {
+        static Full ii = new Full();
+
+        static void Main(string[] args)
+        {
+            for (int z = 0; z < 99999; z++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($" ┌─────────────────────МЕНЮ─────────────────────┐");
+                Console.WriteLine($" │ 1. Выполнить.                                │");
+                Console.WriteLine($" │ 2. Вывести.                                  │");
+                Console.WriteLine($" │ 3. Очистить консоль.                         │");
+                Console.WriteLine($" │ 4. Выйти из программы.                       │");
+                Console.WriteLine($" └──────────────────────────────────────────────┘");
+                Console.Write(" Выберите команду: ");
+                int x1 = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine();
+                switch (x1)
+                {
+                    case 1: ii.add(); break;
+                    case 2: ii.print(); break;
+                    case 3: Console.Clear(); break;
+                    case 4: Environment.Exit(0); break;
+                }
+            }
+        }
+    }
+}
