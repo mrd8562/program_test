067e5298823cbaf4bb4abb4796efdad06296a1b1
mrd <qzwxecrvtbasdf@gmail.com>
2020-03-23 16:17:15 +0300

    kvitan1

---
+++ kvitan_v1.1/kvitan/Full.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kvitan
+{
+    class Full
+    {
+        static Full[] a = new Full[500];
+        static int n;
+
+        private int num { get; set; }
+        private string date { get; set; }
+        private float sum { get; set; }
+
+        internal void add()
+        {
+            Console.WriteLine();
+            Console.Write("Введите кол-во записей: ");
+            n = Convert.ToInt16(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = new Full();
+                Console.Write("Номер квитанции: ");
+                a[i].num = int.Parse(Console.ReadLine());
+                Console.Write("Дата: ");
+                a[i].date = Console.ReadLine();
+                Console.Write("Стоимость: ");
+                a[i].sum = Convert.ToSingle(Console.ReadLine());
+                Console.WriteLine();
+            }
+        }
+        internal void print()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Список стран:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{i}. Номер квитанции: {a[i].num}, Дата: {a[i].date}, Стоимость: {a[i].sum}.");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ kvitan_v1.1/kvitan/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kvitan
+{
+    class kvit
+    {
+        static Full ii = new Full();
+
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
